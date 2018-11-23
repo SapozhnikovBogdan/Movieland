ca4b5164944a34e8c9925f58eefc8cd8528ee7ff
bogdan <sapozhnikov.bogdan@gmail.com>
2018-11-23 12:57:18 +0200

    [lg-1] Add file appender. Configured file appender for logback with max log-file size 5MB

--- src/main/java/com/sapozhnikov/movieland/controller/MovieController.java
+++ src/main/java/com/sapozhnikov/movieland/controller/MovieController.java
@@ -2,6 +2,8 @@ package com.sapozhnikov.movieland.controller;
 
 import com.sapozhnikov.movieland.entity.Movie;
 import com.sapozhnikov.movieland.service.MovieService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,15 +13,18 @@ import java.util.List;
 
 @RestController
 public class MovieController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     private MovieService movieService;
 
     @GetMapping(path="/movie", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public List<Movie> getMovies(){
+        logger.info("Get request came by path /movie" );
         return movieService.getAll();
     }
 
     @GetMapping(path="/movie/random", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public List<Movie> getRandomMovies(){
+        logger.info("Get request came by path /movie/random" );
         return movieService.getRandom();
     }
 
